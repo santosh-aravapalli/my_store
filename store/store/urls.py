b7c0386ee0a907f6b8451c5f397985a1982f6041
santosh <santosh.aravapalli@gmail.com>
2021-03-09 13:20:07 -0800

    static files config

--- store/store/urls.py
+++ store/store/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 from office.views import employeeregister, home, productview, orderview, orderdetailview, employeeapiview, officecodeapi
@@ -35,6 +37,6 @@ urlpatterns = [
     path("api/",include(emp_router.urls)),
     path("office/<str:off_code>",officecodeapi,name="off_code_api"),
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
